144cf47737923a8b40cdf2e385b0462371152c17
Jim Hessin <jhessin@gmail.com>
2020-03-03 23:51:19 -0700

    General Updates

--- 999-brew-setup.sh
+++ 999-brew-setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+
+cd brew
+./*.sh
